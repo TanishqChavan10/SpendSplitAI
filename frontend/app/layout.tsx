18b5f0050a3db1865a437177eeba34a9868e277e
TanishqChavan10 <tan20051007@gmail.com>
2026-01-03 00:29:46 +0530

    chore: clean reinstall after React2Shell fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/provider/providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SpendSplit",
@@ -26,10 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
         <Providers>{children}</Providers>
       </body>
+
     </html>
   );
 }
